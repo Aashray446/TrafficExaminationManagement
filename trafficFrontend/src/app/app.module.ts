2daec81918274552cfd5ca679be1233e7d53d280
Aashray Katiyar <aashray446@gmail.com>
2022-10-31 10:58:54 +0530

    <API> : [Auth : Env and  Auth Interceptors]

--- trafficFrontend/src/app/app.module.ts
+++ trafficFrontend/src/app/app.module.ts
@@ -4,14 +4,20 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { NotfoundComponent } from './core/components/notfound/notfound.component';
-
-
+import { AuthInterceptor } from './core/service/auth-interceptor';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 
 @NgModule({
     declarations: [AppComponent, NotfoundComponent],
     imports: [AppRoutingModule, AppLayoutModule],
     providers: [
-        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        { provide: LocationStrategy,
+            useClass: HashLocationStrategy },
+        {
+            provide : HTTP_INTERCEPTORS,
+            useClass : AuthInterceptor,
+            multi : true
+        }
 
     ],
     bootstrap: [AppComponent],
---
+++ trafficFrontend/src/app/core/service/auth-interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+    intercept(req: HttpRequest<any>,
+              next: HttpHandler): Observable<HttpEvent<any>> {
+
+        const idToken = localStorage.getItem("token");
+
+        if (idToken) {
+            const cloned = req.clone({
+                headers: req.headers.set("Authorization",
+                    "Bearer " + idToken)
+            });
+
+            return next.handle(cloned);
+        }
+        else {
+            return next.handle(req);
+        }
+    }
+}
