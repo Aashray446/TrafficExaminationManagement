15cd02f392e31584e6b1de6ebc98add8ebe44eb1
Aashray Katiyar <aashray446@gmail.com>
2022-11-05 19:02:52 +0530

    <Api> : [More User Seeders added]

--- Backend/migrator/seeds/users.js
+++ Backend/migrator/seeds/users.js
@@ -8,12 +8,28 @@ module.exports = {
 
 async function _run() {
 	try {
-		const exampleUserData = {
-			email: "aashray446@gmail.com",
-			password: "12345678"
+		const exampleUserData = [
+			{
+				email: "aashray446@gmail.com",
+				password: "12345678",
+				role: 'Admin'
+			},
+			{
+				name: "Mid Officer",
+				email: "mid@gmail.com",
+				password: "12345678",
+				role: "MidLevelOfficer"
+			}
+		]
+
+		for (const element of exampleUserData) {
+			await usersFacade.register(element)
 		}
 
-		const user = await usersFacade.register(exampleUserData);
+		// exampleUserData.forEach(async element => {
+		// 	await usersFacade.register(element);
+		// });
+
 	}
 	catch (error) {
 		return Promise.reject(error);
