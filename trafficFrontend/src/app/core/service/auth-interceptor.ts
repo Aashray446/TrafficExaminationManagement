bf528b56681305548eb9a796ddb0005cb2caa4c4
Aashray Katiyar <aashray446@gmail.com>
2022-11-10 11:29:29 +0530

    <Fix> : [Env variable]

--- trafficFrontend/src/app/core/service/auth-interceptor.ts
+++ trafficFrontend/src/app/core/service/auth-interceptor.ts
@@ -1,7 +1,6 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -11,14 +10,6 @@ export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>,
               next: HttpHandler): Observable<HttpEvent<any>> {
 
-
-            if(!environment.production) {
-                   const cloned = req.clone({
-                    headers: req.headers.set("Authorization",
-                        "Bearer " + environment.token)
-                });
-                    return next.handle(cloned)
-                }
 
         const idToken = localStorage.getItem("token");
 
