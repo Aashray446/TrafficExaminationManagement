7bf840787fe8cc14032a8f66082fdc9353f928ae
Aashray Katiyar <aashray446@gmail.com>
2022-11-12 21:00:42 +0530

    <Fix> : [Admin Applicant Details Submit]

--- Backend/app/facades/applicant.js
+++ Backend/app/facades/applicant.js
@@ -43,21 +43,39 @@ async function _create(ApplicantDetails) {
     }
 }
 
-async function _update(ApplicantDetails) {
+async function _update(applicant) {
     try {
         // Try to find user.
-
-        const result = await Applicant.update(
-            { name: ApplicantDetails.name, serialNumber: ApplicantDetails.serialNumber, tokken: ApplicantDetails.tokken, photo: ApplicantDetails.photo, applicantDetails: ApplicantDetails.applicantDetails },
-            { where: { applicantId: ApplicantDetails.applicantId } }
+        let result = await Applicant.update(
+            { name: applicant.name, serialNumber: applicant.serialNumber, tokken: applicant.tokken, photo: applicant.photo },
+            { where: { applicantId: applicant.applicantId } }
         );
-
         //  If user not found, throw error with name UserNotFound:
-        if (result[0] == 0) {
+        if (result == 0) {
             const err = new Err('Applicant not found');
             err.name = "ApplicantNotFound";
             throw err;
         }
+
+        const applicantDetails = applicant.applicantDetails
+        result = await ApplicantDetails.update(
+            {
+                eightPattern: applicantDetails.eightPattern,
+                trafficLightPattern: applicantDetails.trafficLightPattern,
+                rampPattern: applicantDetails.rampPattern,
+                lParkingPattern: applicantDetails.lParkingPattern,
+                behaviourPattern: applicantDetails.behaviourPattern,
+            }
+            ,
+            { where: { ApplicantApplicantId: applicant.applicantId } }
+        );
+
+        if (result == 0) {
+            const err = new Err('Applicant Details not found');
+            err.name = "ApplicantDetailsNotFound";
+            throw err;
+        }
+
 
         // Send output.
         return Promise.resolve([result]);
